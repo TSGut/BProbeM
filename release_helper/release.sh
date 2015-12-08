d69518f69fe35f0252480562b359a00973bfddf1
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2015-12-08 18:17:53 +0100

    fix usage of 'tag'

--- release_helper/release.sh
+++ release_helper/release.sh
@@ -1,9 +1,9 @@
 # TODO: change version in "project.json"
 
 # create a git tag $tag before doing this
-tag="v0.1.0-alpha"
+tag="v0.1.0"
 
-git archive --format tar.gz v0.1.0-alpha --output /tmp/BProbe-${tag}.tar.gz --prefix=BProbe/
+git archive --format tar.gz ${tag} --output /tmp/BProbe-${tag}.tar.gz --prefix=BProbe/
 
 # TODO:
 # remove "Install" folder
