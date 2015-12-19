63d49c37a399e42fbe3a0e3bc8db01e875ab30ea
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2015-12-19 21:47:57 +0100

    add profiler package

---
+++ Profiler.m
@@ -0,0 +1,57 @@
+(* ::Package:: *)
+ 
+BeginPackage["Profiler`"];
+
+	AddRecord::usage="";
+	ShowChart::usage="";
+	
+
+Begin["`Private`"];
+
+	(* initialize *)
+	profiles = <||>;
+
+	SetAttributes[AddRecord, HoldRest];
+	AddRecord[id_, expr_] := Block[{timing},
+	
+		(* create id if doesnt exist *)
+		If[!VectorQ[profiles[id]],
+			AppendTo[profiles, id -> {}];
+		];
+		
+		timing = Timing[expr];
+		AppendTo[profiles[id],timing[[1]]];
+		
+		Return[timing[[2]]];
+	
+	];
+	
+	ShowChart[] := Block[{bc},
+		bc = BarChart[
+			(Tooltip[
+				GetTotalTime[#],
+				GetText[#]
+			])& /@ Keys[profiles],
+			ChartLegends -> Keys[profiles],
+			ChartStyle -> "DarkRainbow",
+			PlotTheme -> "Business"
+		];
+
+		Return[bc];	
+	];
+	
+	GetText[id_] := Block[{text},
+		text = "_" <> TextString[id] <> "_\n"
+			<> "Mean:\t(" <> TextString[GetMean[id]] <>  " \[PlusMinus] "
+			<> TextString[GetStandardDeviation[id]] <> ") s\n"
+			<> "\[NumberSign] calls:\t" <> TextString[GetNumberOfCalls[id]];
+		Return[text];
+	];
+
+	GetMean[id_] := Mean[profiles[id]];
+	GetStandardDeviation[id_] := StandardDeviation[profiles[id]];
+	GetNumberOfCalls[id_] := Length[profiles[id]];
+	GetTotalTime[id_] := Total[profiles[id]];
+
+End[];
+EndPackage[];
