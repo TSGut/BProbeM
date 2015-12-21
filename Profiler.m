6c0aacd60f7b06c224c801c6e5f20f38b47ded65
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2015-12-21 12:13:49 +0100

    Profile.m: add GPL-header

--- Profiler.m
+++ Profiler.m
@@ -1,5 +1,28 @@
 (* ::Package:: *)
- 
+
+(*
+	Copyright 2015 Lukas Schneiderbauer (lukas.schneiderbauer@gmail.com)
+
+
+    This file is part of BProbe.
+
+    BProbe is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    BProbe is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with BProbe.  If not, see <http://www.gnu.org/licenses/>.
+
+*)
+
+
+
 BeginPackage["BProbe`Profiler`"];
 
 	AddRecord::usage="";
