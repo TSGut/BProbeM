7e9ce611bc069607be04cb3500a1d5f5fb885b8b
TSGut <34382069+TSGut@users.noreply.github.com>
2018-01-15 20:48:42 +0100

    removed unnecessary filed

--- release_helper/update_version.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-
-require 'json'
-
-json = JSON.load(open("../project.json"))
-json["version"] = ARGV[0]
-
-File.open("../project.json", 'w') { |file| file.write(JSON.pretty_generate(json)) }
